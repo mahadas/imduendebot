0b89cf895a52af6e9e99e889bf395bd42ec80bcb
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-26 13:59:50 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,41 +14,5 @@ client.on("ready", () => {
 });
 
 
-client.on("message", (message) => {
-	const args = message.content.slice(prefix.length).trim().split(/ +/g);
-	const command = args.shift().toLowerCase();	
-	if (command === 'conexion') {
-		let ping = Math.floor(message.client.ping);
-		message.channel.send({embed: {
-				color: 7419530,
-				description: `:satellite_orbital: Tu conexion es de ${ping} ms`
-			}});	
-	}
-	if (message.content.startsWith(prefix + "generar")) {
-		if (message.channel.id === '626559477566406666') {	
-			if (args[0] === 'minecraft') {
-				var minecrafts = ["Your facts", "...", "..."];
-				var mine = Math.floor(Math.random() * minecrafts.length);
-				message.author.sendMessage(minecrafts[mine]) return
-				message.channel.send({embed: {
-					color: 1752220,
-					description: 'La cuenta se te envio al mensaje privado.'
-				}});	
-			}
-		}		
-		else {
-			message.channel.send({embed: {
-				color: 15158332,
-				description: 'No puedes usar este comando en este canal.'
-			}});			
-
-		}
-	}
-
-
-});
-
-
-
 
 client.login(process.env.BOT_TOKEN);
