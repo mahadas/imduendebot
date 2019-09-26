cca37023c1a2b6ef66750e61b85e4bbc215701fa
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-26 14:12:00 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on("ready", () => {
 
 client.on("message", (message) => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/g);
-	const command = args.shift().toLowerCase();	
+	const command = args.shift().toLowerCase();
 	if (command === 'conexion') {
 		let ping = Math.floor(message.client.ping);
 		message.channel.send({embed: {
@@ -24,7 +24,33 @@ client.on("message", (message) => {
 				description: `:satellite_orbital: Tu conexion es de ${ping} ms`
 			}});
 	}
+	if (command === prefix + 'ayuda') {
+		message.channel.send({embed: {
+			color: 10181046,
+      	author: {
+			name: "Aca te estoy ayudando en lo que necesites",
+			icon_url: "https://i.imgur.com/Lote53F.jpg"
+		},
+		fields: [{
+			name: ".Redes",
+			value: "Aca puedes ver las redes sociales de ImDuende_"
+		},
+		{
+			name: ".Conexion",
+			value: "Ver conexion en el discord de ImDuende_"
+		},
+		],
+		timestamp: new Date(),
+		footer: {
+		icon_url: client.user.avatarURL,
+		text: "Diseñado por mahada#0641"
+      }
+    }
+
+
 });
+
+
 
 
 
