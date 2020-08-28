a5f3c88ec83e7c204dd9819971d5ecc76bb1f82a
mahadas <54777320+mahadas@users.noreply.github.com>
2020-08-27 21:38:57 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on("ready", () => {
 	client.user.setPresence({
        status: "online",
        game: {
-           name: "!ayuda | Brunenger",
+           name: "+ayuda | Brunenger",
            url: "https://www.twitch.tv/Brunenger",
            type: "STREAMING"
        }
@@ -25,6 +25,27 @@ client.on("message", (message) => {
 			}});
 		return;
 	}
+	if (command === "ausente") {
+	    if (message.channel.id === '621353649729306634') {
+	     message.delete();
+	     var ahora = new Date();
+	     var minuto = ahora.getUTCMinutes();  
+	     var hora = ahora.getUTCHours() + 2;
+	     var segundo = ahora.getUTCSeconds();
+	     let mes = [`Enero`,`Febrero`,`Marzo`,`Abril`,`Mayo`,`Junio`,`Julio`,`Agosto`,`Septiembre`,`Octubre`,`Noviembre`,`Diciembre`];
+	     let suffix = [`st`,`nd`,`rd`];
+	     const darpresente = new Discord.RichEmbed()
+	     .setTitle("Proxima Invasion:", 'https://i.imgur.com/FwiJzpS.png')
+	     .addField("Dia:", `  ${message.author}`)
+	     .addField(" ")   
+	     .addField("Hora:", `  ${hora}h:${minuto}m:${segundo}s - Hora España`)
+	     .addField("Dia:", `  ${ahora.getDate()} de ${mes[ahora.getMonth()]} de 2019 `)
+	     .setColor(15158332)
+	     .setThumbnail(message.author.avatarURL)
+	     .setFooter("Canal De Twitch: Brunenger", 'https://i.imgur.com/FwiJzpS.ng');
+	     client.channels.get('621353649729306634').send(darpresente);
+	      }
+  }   
 
 });
 
