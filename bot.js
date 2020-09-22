099d9140cda79cb0c226bc7bf55a4465a2469fa9
mahadas <54777320+mahadas@users.noreply.github.com>
2020-09-22 09:53:30 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -104,8 +104,37 @@ client.on("message", (message) => {
 		.setFooter(`${message.author.tag}`, message.author.avatarURL)
 		message.channel.sendEmbed(embed);
 	}
+  
 
-	if (message.content.startsWith(prefix + "invasion")) {
+    if (message.content.startsWith(prefix + "kick")) {	
+    // This command must be limited to mods and admins. In this example we just hardcode the role names.
+    // Please read on Array.some() to understand this bit: 
+    // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
+    if(!message.member.roles.some(r=>["ADMIN", "Moderator"].includes(r.name)) )
+      return message.reply( message.author + "No tienes permiso para esto!");
+    
+    // Let's first check if we have a member and if we can kick them!
+    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
+    // We can also support getting the member by ID, which would be args[0]
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("Menciona un miembro que este dentro del servidor");
+    if(!member.kickable) 
+      return message.reply("¡No puedo kickear a este usuario! ¿Tienen un papel más alto? ¿Tengo permisos de kick?");
+    
+    // slice(1) removes the first part, which here should be the user mention or ID
+    // join(' ') takes all the various parts to make it a single string.
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "Sin razon";
+    
+    // Now, time for a swift kick in the nuts!
+    await member.kick(reason)
+      .catch(error => message.reply(`Perdon ${message.author} No pude kickear por: ${error}`));
+    message.reply(`${member.user.tag} ha sido kickeado por ${message.author.tag} porque: ${reason}`);
+
+  }
+
+   	if (message.content.startsWith(prefix + "invasion")) {
 
 		message.delete();
 		var ahora = new Date();	
