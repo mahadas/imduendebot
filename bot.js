6b52e395cf61fc9ab43ef08bc1668ad2bc2da301
mahadas <54777320+mahadas@users.noreply.github.com>
2020-09-28 01:27:33 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,7 +52,8 @@ client.on("message", (message) => {
 
 	}
 	if (message.channel.id === '756310874905378907') {
-		client.channels.get('759992232744189973').send(`${message.author.tag}`);
+		let msg = message.author.send
+		client.channels.get('759992232744189973').send(`${message.author.tag}` + `${msg}`);
 	}
 
 
